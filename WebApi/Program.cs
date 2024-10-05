122d2d6dd9b08840f9dc7e2f28f4bf9bc49be01f
Yannick Nwankwo <117281275+Yannick-Nw@users.noreply.github.com>
2024-10-05 20:51:26 +0200

    web ui v0.4

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -2,6 +2,20 @@ using WebApi.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddLogging();
+
+// CORS konfigurieren, um Anfragen von localhost:80 (WebUI) zuzulassen
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowWebUI",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost") // Die URL deiner Web-UI
+                .AllowAnyHeader()
+                .AllowAnyOrigin()
+                .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ILogger>(prov => prov.GetRequiredService<ILoggerFactory>().CreateLogger("Default"));
@@ -14,7 +28,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Verwende die CORS-Policy
+app.UseCors("AllowWebUI");
+
 // Map endpoints
 app.MapDocumentEndpoints();
+
+//app.MapGet("/", () => Results.Redirect("/index.html")); // Weiterleitung auf die Hauptseite
 
 app.Run();
